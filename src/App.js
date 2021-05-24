2231baa3e08e89db06abc393ee333d1973b14887
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-05-24 11:50:08 -0500

    Set up fetching data from randomUser API.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+// const useFetch = url => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       await fetch(url)
+//         .then(response => response.json())
+//         .then(data => setData(data))
+//         .catch(error => console.error(error));
+//     };
+//     fetchData();
+//   }, url);
+  
+//   console.log("useFetch returns: ", data);
+//   return data;
+// };
+
 function App() {
+  const [users, setUsers] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+        .catch(error => console.error(error));
+    };
+    fetchData();
+  }, []);
+
+  console.log("users", users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Directory</h1>
+      <table>
+      {users.results &&
+        users.results.map(user => {
+          <>
+          <tr>
+            <th>Picture</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+            <image src={user.picture.medium} alt="thumbnail"/>
+            <h2>{user.name.first + " " + user.name.last}</h2>
+
+          </>
+        })
+      }
+      </table>
     </div>
   );
 }
